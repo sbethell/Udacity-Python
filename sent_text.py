63f72ab8297011844870fa6adc48f39bb81804d1
Sebastian Bethell <sbethell01@hotmail.com>
2017-05-07 13:30:43 -0700

    Add files via upload
    Send texts through Twilio

---
+++ sent_text.py
@@ -0,0 +1,15 @@
+from twilio.rest import TwilioRestClient
+
+# Your Account SID from twilio.com/console
+account_sid = "AC0c87692e63cbea74751dd3cc2aeddca6"
+# Your Auth Token from twilio.com/console
+auth_token  = "c6d91d3053074202b570f4397d5dc9f6"
+
+client = TwilioRestClient(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+12505320540", 
+    from_="+15874094518",
+    body="I love Sophia Ariana Mayen! She's a babe!!")
+
+print(message.sid)
